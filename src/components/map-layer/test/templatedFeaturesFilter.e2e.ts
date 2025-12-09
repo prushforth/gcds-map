6abf86783c2d1bfe126c58f286e42cfae21eeb52
Peter Rushforth <peter.rushforth@nrcan-rncan.gc.ca>
2025-12-09 10:45:07 -0500

    Add initial refactoring of templatedFeaturesFilter.e2e.ts and html

---
+++ src/components/map-layer/test/templatedFeaturesFilter.e2e.ts
@@ -0,0 +1,63 @@
+import { test, expect, chromium } from '@playwright/test';
+
+test.describe('Simple query by select values without map extent filter tests', () => {
+  let page;
+  let context;
+  test.beforeAll(async () => {
+    context = await chromium.launchPersistentContext('', { slowMo: 250 });
+    page = await context.newPage();
+    await page.goto('/test/map-layer/templatedFeaturesFilter.html');
+  });
+
+  test.afterAll(async function () {
+    await context.close();
+  });
+  test('All features loaded at start', async () => {
+    const features = await page.locator('map-feature');
+    expect(features).toHaveCount(8);
+  });
+  test('User can select/filter features by category', async () => {
+    const restaurants = await page.locator('map-feature');
+    expect(restaurants).toHaveCount(8);
+
+    await page.hover('.leaflet-top.leaflet-right > div');
+    await page.click(
+      'css= body > gcds-map >> css= div > button.mapml-layer-item-settings-control.mapml-button'
+    );
+    await page.getByRole('button', { name: 'Sub-layer Settings' }).click();
+    await page.locator('summary').filter({ hasText: 'cuisine' }).click();
+    await page.selectOption(
+      'css= body > gcds-map >> css= details:nth-child(2).mapml-control-layers select',
+      'italian'
+    );
+    await page.waitForTimeout(250);
+    expect(restaurants).toHaveCount(1);
+  });
+  test('<map-select> <map-option> attributes are copied to layer control <option> elements', async () => {
+    await page.selectOption(
+      'css= body > gcds-map >> css= details:nth-child(2).mapml-control-layers select',
+      'restaurants'
+    );
+    await page.waitForTimeout(250);
+    const firstOptionSelected = await page.$eval(
+      'css= gcds-map details.mapml-control-layers select option:nth-child(1)',
+      (option) => option.selected
+    );
+    expect(firstOptionSelected).toBeTruthy();
+    const firstOptionLabel = await page.$eval(
+      'css= gcds-map details.mapml-control-layers select option:nth-child(1)',
+      (option) => option.label
+    );
+    expect(firstOptionLabel).toEqual('All cuisines');
+    const firstOptionValue = await page.$eval(
+      'css= gcds-map details.mapml-control-layers select option:nth-child(1)',
+      (option) => option.value
+    );
+    expect(firstOptionValue).toEqual('restaurants');
+    const thirdOptionValue = await page.$eval(
+      'css= gcds-map details.mapml-control-layers select option:nth-child(3)',
+      (option) => option.value
+    );
+    expect(thirdOptionValue).toEqual('african');
+  });
+});
