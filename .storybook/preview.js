ec8e1be29f2319c31f17a5d45b6ffc5835e65cae
Peter Rushforth <peter.rushforth@nrcan-rncan.gc.ca>
2026-01-29 14:31:35 -0500

    Upgrade to gcds-components 0.47.0 peer dep Add reference to CDN version of gcds.css in storybook story preview-head.html Import and define gcds components in preview.js for use (of gcds-link)  in story overview.mdx

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,10 @@
 import {defineCustomElements} from '../dist/esm/loader';
+import {defineCustomElements as defineGcdsComponents} from '@cdssnc/gcds-components/loader';
 
 import gcdsTheme from './gcds-theme';
 
 defineCustomElements();
+defineGcdsComponents();
 
 const preview = {
   parameters: {
